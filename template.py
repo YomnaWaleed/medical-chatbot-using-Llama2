4e9f706d19a2fdc3781df232f962232da46f5423
LAPSHOP <yomnawaleed2022@gmail.com>
2025-03-15 01:37:38 +0200

    temp

---
+++ template.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] : %(message)s:')
+
+list_of_files = [
+  "src/__init__.py",
+  "src/helper.py",
+  "src/prompt.py",
+  ".env",
+  "setup.py",
+  "research/trials.ipynb",
+  "app.py",
+  "store_index.py",
+  "static",
+  "templates/chat.html",  
+  
+]
+
+for filepath in list_of_files:
+  Path()
