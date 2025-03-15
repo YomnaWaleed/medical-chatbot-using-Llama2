56939d89a3d5f8a85ba795a00a606787489d51be
LAPSHOP <yomnawaleed2022@gmail.com>
2025-03-15 06:01:00 +0200

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name= 'Medical chatbot',
+  version= '0.0.0',
+  author= 'yomna waleed',
+  author_email='yomnawaleed2002@gmail.com', 
+  packages= find_packages(),
+  install_requires = []
+)
