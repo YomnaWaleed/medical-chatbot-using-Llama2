df58562736a84156fdea46bb82ede9ffedfb9e39
LAPSHOP <yomnawaleed2022@gmail.com>
2025-03-15 07:04:36 +0200

    template added

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain.vectorstores import FAISS
+
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+
+from src.prompt import *
+import os
+
+
+app = Flask(__name__)
+
+embeddings = download_hugging_face_embeddings()
+vectorstore = FAISS.load_local("faiss_index", embeddings)
+
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain_type_kwargs = {"prompt": PROMPT}
+
+llm = CTransformers(
+    model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+    model_type="llama",
+    config={'max_new_tokens': 512, 'temperature': 0.8}
+)
+
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs
+)
+
+@app.route('/')
+def home():
+    return render_template('chat.html')
+  
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="8080",debug=True)
